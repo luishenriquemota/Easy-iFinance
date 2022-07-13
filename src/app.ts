294d8bb625ebf7c2bf33fd77d8092ce573d5c549
Luis Henrique Mota <71797338+luishenriquemota@users.noreply.github.com>
2022-07-13 13:43:00 -0300

    created Branch develop with home folders

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port)
+})
