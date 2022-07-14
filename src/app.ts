23363e5dfd9e70ee985c9325c7937cf03b64958d
Luis Henrique Mota <71797338+luishenriquemota@users.noreply.github.com>
2022-07-14 10:30:11 -0300

    feat: created middlewares authToken and errors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express"
+import { errorMiddleware } from "./middlewares/error.middleware"
 
 const app = express()
 
 app.use(express.json())
 
-
+app.use(errorMiddleware)
 
 const port = process.env.PORT || 3001
-
 app.listen(port, () => {
   console.log("Rodando na porta " + port)
 })
