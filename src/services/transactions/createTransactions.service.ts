7718f9e8168d53db77c6ad9c331ff86962e15a5d
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-19 13:52:45 -0300

    Feat: create email request for transactions

--- src/services/transactions/createTransactions.service.ts
+++ src/services/transactions/createTransactions.service.ts
@@ -4,31 +4,52 @@ import { Transactions } from "../../entities/transactions.entity";
 import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors/appError";
 import { ITransaction } from "../../interfaces/Transactions";
+import { IEmailRequest } from "../../interfaces/emails";
+import { sendEmail } from "../../utils/sendEmail.util";
 
+const createTransactionsService = async (
+  foundUser: User,
+  foundCard: Card,
+  { description, category, value, type }: ITransaction,
+) => {
+  const transactionsRepository = AppDataSource.getRepository(Transactions);
 
+  const newTransaction = new Transactions();
+  newTransaction.description = description;
+  newTransaction.category = category;
+  newTransaction.value = value;
+  newTransaction.type = type;
+  newTransaction.card = foundCard;
+  newTransaction.user = foundUser;
 
-const createTransactionsService = async (foundUser:User, foundCard:Card,{description,category,value, type}:ITransaction) => {
-    const transactionsRepository = AppDataSource.getRepository(Transactions)
+  const emailData: IEmailRequest = {
+    subject: "Relatorio da transação",
+    text: `<h1>Transação criada.</h1>
+    <h2>${foundCard.name}</h2>
+    <ul>
+        <h3>${newTransaction.category}</h3>
+            <li>
+                <p>${newTransaction.description}</p>
+                <p>${newTransaction.value}</p>
+                <p>${newTransaction.type}</p>
+            </li>
+    </ul>
+    `,
+    to: foundUser.email
+  }
+  await sendEmail(emailData)
 
-    const newTransaction = new Transactions
-    newTransaction.description = description
-    newTransaction.category = category
-    newTransaction.value = value
-    newTransaction.type =type
-    newTransaction.card = foundCard
-    newTransaction.user = foundUser
-   
-    await transactionsRepository.save(newTransaction)
+  await transactionsRepository.save(newTransaction);
 
-    const returingTransaction = {
-        transactions_id: newTransaction.transactions_id,
-        description: newTransaction.description,
-        value: newTransaction.value,
-        type: newTransaction.type,
-        cardId: newTransaction.card.id,
-        userId: newTransaction.user.id
-    } 
-    return returingTransaction
-}
+  const returingTransaction = {
+    transactions_id: newTransaction.transactions_id,
+    description: newTransaction.description,
+    value: newTransaction.value,
+    type: newTransaction.type,
+    cardId: newTransaction.card.id,
+    userId: newTransaction.user.id,
+  };
+  return returingTransaction;
+};
 
-export default createTransactionsService
+export default createTransactionsService;
